6a4aab60a03bae5ba04ab4d265e56ed4e3cd7aed
Bui The Hoa <buithehoa@gmail.com>
2017-03-25 09:36:04 +0700

    rearrange * Integrate with tests

--- Crossover/rearrange.rb
+++ Crossover/rearrange.rb
@@ -2,22 +2,22 @@
 
 # DO NOT MODIFY anything outside the below function
 def rearrange(elements)
-    elements.sort do |a, b|
-      a_1_count = a.to_s(2).count('1')
-      b_1_count = b.to_s(2).count('1')
+  elements.sort do |a, b|
+    ai = a.to_i
+    bi = b.to_i
 
-      if a_1_count == b_1_count
-        a <=> b
-      else
-        a_1_count <=> b_1_count
-      end
+    a_1_count = ai.to_s(2).count('1')
+    b_1_count = bi.to_s(2).count('1')
+
+    if a_1_count == b_1_count
+      ai <=> bi
+    else
+      a_1_count <=> b_1_count
     end
+  end
 end
 # DO NOT MODIFY anything outside the above function
 
-puts rearrange([ 5, 3, 7, 10, 14, 9 ])
-
-=begin
 _elements_cnt = 0
 _elements_cnt = Integer(gets)
 _elements_i=0
@@ -33,4 +33,3 @@ res = rearrange(_elements);
 for res_i in res do
   puts res_i
 end
-=end
