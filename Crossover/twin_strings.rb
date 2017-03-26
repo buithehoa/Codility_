dd1b985bb0cb15648780b7a6c08dca4f90eda316
Bui The Hoa <buithehoa@gmail.com>
2017-03-26 10:47:15 +0700

    Twin Strings * Implement the swap

--- Crossover/twin_strings.rb
+++ Crossover/twin_strings.rb
@@ -2,13 +2,40 @@
 
 # DO NOT MODIFY anything outside the below function
 def twins(a, b)
+  results = []
 
-    return ["Yes","No"];
+  for i in 0...a.length do
+    if a[i].length != b[i].length
+      results.push("No")
+    else
+      a_chars = a[i].chars
+      b_chars = b[i].chars
 
+      a_even_chars, b_even_chars, a_odd_chars, b_odd_chars = [], [], [], []
+      for j in 0...a_chars.length do
+        if j % 2 == 0
+          a_even_chars.push(a_chars[j])
+          b_even_chars.push(b_chars[j])
+        else
+          a_odd_chars.push(a_chars[j])
+          b_odd_chars.push(b_chars[j])
+        end
+      end
+
+      if a_even_chars.sort == b_even_chars.sort && a_odd_chars.sort == b_odd_chars.sort
+        results.push("Yes")
+      else
+        results.push("No")
+      end
+    end
+  end
+
+  results
 end
 # DO NOT MODIFY anything outside the above function
 
 # DO NOT MODIFY THE CODE BELOW THIS LINE!
+=begin
 _a_cnt = Integer(gets)
 _a_i=0
 _a = Array.new(_a_cnt)
@@ -33,3 +60,4 @@ res = twins(_a, _b);
 for res_i in res do
   puts res_i
 end
+=end
