5509b9f942d81d2ea5c9479e5a2c5215ab8cd51e
Bui The Hoa <buithehoa@gmail.com>
2020-08-01 12:59:01 +0700

    Add a Go solution.

---
+++ Codility/Lessons/02. Arrays/OddOccurrencesInArray/solution.go
@@ -0,0 +1,27 @@
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+import "sort"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+    // write your code in Go 1.4
+    unpaired := 0
+    sort.Ints(A)
+
+    i := 0
+    for unpaired == 0 || i < len(A) {
+        if A[i] == A[i + 1] {
+            i += 2
+        } else {
+            unpaired = A[i]
+            i += 1
+        }
+    }
+
+    return unpaired
+}
