fd981974acd74be7fa540729a8d59ae4d02155ad
Bui The Hoa <buithehoa@gmail.com>
2020-08-01 12:13:51 +0700

    Add solution for  OddOccurrencesInArray.

---
+++ Codility/Lessons/02. Arrays/OddOccurrencesInArray/solution.rb
@@ -0,0 +1,22 @@
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def solution(a)
+  # write your code in Ruby 2.2
+
+  unpaired = 0
+  sorted = a.sort
+  i = 0
+  loop do
+    if sorted[i] == sorted[i+1]
+      i += 2
+    else
+      unpaired = sorted[i]
+      i += 1
+    end
+
+    break if unpaired > 0 || i >= sorted.length
+  end
+
+  unpaired
+end
