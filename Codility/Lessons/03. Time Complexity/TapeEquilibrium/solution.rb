75705d298d1a77f4eeaaab2f6a4112cb207fb9f9
Bui The Hoa <buithehoa@gmail.com>
2021-02-09 19:12:57 +0700

    Optimize #diff.

--- Codility/Lessons/03. Time Complexity/TapeEquilibrium/solution.rb
+++ Codility/Lessons/03. Time Complexity/TapeEquilibrium/solution.rb
@@ -3,27 +3,22 @@
 
 def solution(a)
   # write your code in Ruby 2.2
-  min = diff(a, 1)
+  head = a[0]
+  tail = a[1..(a.length - 1)].reduce(:+)
+  min = (head - tail).abs
 
   (2...a.length).each do |p|
-    tmp = diff(a, p)
-    min = tmp if min > tmp
+    tmp = diff(a, p, head, tail)
+    min = tmp if tmp < min
     break if min.zero?
   end
 
   min
 end
 
-def diff(a, p)
-  head = 0
-  for i in 0..(p - 1) do
-    head += a[i]
-  end
+def diff(a, p, head, tail)
+  new_head = head + a[p]
+  new_tail = tail - a[p]
 
-  tail = 0
-  for i in p..(a.length - 1) do
-    tail += a[i]
-  end
-
-  (head - tail).abs
+  (new_head - new_tail).abs
 end
