6bafad88447ff4ab81a86c153b91bee0fc48f9bf
Bui The Hoa <buithehoa@gmail.com>
2021-03-29 16:43:57 +0700

    Reduce number of steps.

--- Codility/lessons/4-counting_elements/max_counters/solution.rb
+++ Codility/lessons/4-counting_elements/max_counters/solution.rb
@@ -5,6 +5,7 @@ def solution(n, a)
   # write your code in Ruby 2.2
   counters = Array.new n, 0
   max_count = 0
+  max_operation = n + 1
 
   a.each_with_index do |value, index|
     count = 0
@@ -12,8 +13,8 @@ def solution(n, a)
     if value >= 1 && value <= n
       count = counters[value - 1] + 1
       counters[value - 1] = count
-    elsif value == n + 1
-      counters = Array.new(n, max_count)
+    elsif value == max_operation && a[index - 1] != max_operation
+      counters = counters.collect { max_count }
     end
 
     max_count = count if max_count < count
