088e851eeae59aaf31627d9094d8d635e749a243
Bui The Hoa <buithehoa@gmail.com>
2021-03-30 15:00:54 +0700

    Handle max operation with less loops.

--- Codility/lessons/4-counting_elements/max_counters/solution.rb
+++ Codility/lessons/4-counting_elements/max_counters/solution.rb
@@ -5,20 +5,23 @@ def solution(n, a)
   # write your code in Ruby 2.2
   counters = Array.new n, 0
   max_count = 0
+  next_max_count = 0
   max_operation = n + 1
 
   a.each_with_index do |value, index|
     count = 0
 
     if value >= 1 && value <= n
-      count = counters[value - 1] + 1
+      count = counters[value - 1] < max_count ? max_count + 1 : counters[value - 1] + 1
       counters[value - 1] = count
-    elsif value == max_operation && a[index - 1] != max_operation
-      counters = counters.collect { max_count }
+      next_max_count = count if max_count < count
+    elsif value == max_operation
+      max_count = next_max_count
     end
 
-    max_count = count if max_count < count
   end
+
+  counters = counters.collect { |counter| counter < max_count ? max_count : counter }
 
   counters
 end
