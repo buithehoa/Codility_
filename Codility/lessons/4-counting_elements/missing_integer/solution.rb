d766c8e227cc63f85be814ef13a4d5cb16a97bf6
Bui The Hoa <buithehoa@gmail.com>
2021-03-31 14:15:10 +0700

    Add Ruby solution.

---
+++ Codility/lessons/4-counting_elements/missing_integer/solution.rb
@@ -0,0 +1,18 @@
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def solution(a)
+  # write your code in Ruby 2.2
+  a.sort!
+  missing = 1
+
+  a.each do |i|
+    if missing == i
+      missing += 1
+    elsif missing < i
+      break
+    end
+  end
+
+  missing
+end
