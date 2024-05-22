a5f5c4b484b39b1d80e5d04a3aabf8b890ff8e4a
Bui The Hoa <buithehoa@gmail.com>
2024-05-22 16:43:57 +0700

    Add passing cars lesson.

---
+++ lessons/5-prefix_sums/passing_cars/solution.rb
@@ -0,0 +1,20 @@
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def solution(a)
+  # Implement your solution here
+  east_count = 0
+  passing_cars = 0
+
+  a.each do |car|
+    if car == 0
+      east_count += 1
+    else
+      passing_cars += east_count
+      return -1 if passing_cars >= 1_000_000_000
+    end
+  end
+
+  passing_cars
+end
+
