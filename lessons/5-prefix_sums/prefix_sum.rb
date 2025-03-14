27b116cd7dc9eea731b78e4bdb78ea2c848ca6f4
Bui The Hoa <buithehoa@gmail.com>
2025-03-14 14:46:10 +0700

    Add puts for debugging.

--- lessons/5-prefix_sums/prefix_sum.rb
+++ lessons/5-prefix_sums/prefix_sum.rb
@@ -12,18 +12,27 @@ end
 
 # Find the number of contiguous subarrays that sum to the target
 def subarray_sum(arr, target)
+  puts "arr #{arr}"
+
   prefix_sum = calculate_prefix_sum(arr)
   puts "prefix_sum #{prefix_sum}"
   puts
   
   count = 0
   sum_map = {0 => 1}  # Initialize with 0 sum
+  puts "step 0"
+  puts "sum_map #{sum_map}"
+  puts
   
   (1...prefix_sum.length).each do |i|
     puts "step #{i}"
   
     complement = prefix_sum[i] - target
+    puts "prefix_sum[#{i}] #{prefix_sum[i]}, target #{target}, complement #{complement}"
+    
     count += sum_map[complement] if sum_map.key?(complement)
+    puts "count #{count}"
+    
     sum_map[prefix_sum[i]] = (sum_map[prefix_sum[i]] || 0) + 1
     
     puts "sum_map #{sum_map}"
