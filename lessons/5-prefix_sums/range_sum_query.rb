4859b44e04e3a0c2312fe7e8e738a1302262115f
Bui The Hoa <buithehoa@gmail.com>
2025-05-07 17:18:35 +0700

    Refactor prefix sum calculation into its own class.

--- lessons/5-prefix_sums/range_sum_query.rb
+++ lessons/5-prefix_sums/range_sum_query.rb
@@ -1,12 +1,4 @@
-def calculate_prefix_sum(arr)
-  prefix_sum = Array.new(arr.length + 1, 0)
-
-  arr.each_with_index do |value, index|
-    prefix_sum[index + 1] = prefix_sum[index] + value
-  end
-
-  prefix_sum
-end
+require_relative "prefix_sum"
 
 def query_range_sum(arr, from, to)
   prefix_sum = calculate_prefix_sum arr
