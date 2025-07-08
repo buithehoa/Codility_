327193aa0e73f4dfc5f62407b2f9c91d29b2fdc2
Bui The Hoa <buithehoa@gmail.com>
2025-07-08 22:44:50 +0700

    Rollback tests.

--- Lessons/6-sorting/max_product_of_three/solution_test.rb
+++ Lessons/6-sorting/max_product_of_three/solution_test.rb
@@ -1,11 +1,13 @@
 require 'minitest/autorun'
-# require 'minitest/reporters'
+require 'minitest/reporters'
 require_relative 'solution'
 
-# Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
+Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
-class SolutionTest < Minitest::Test
-  def test_calculates_the_result
-    assert_equal 4446, solution(0)
+describe 'solution' do
+  it 'calculates the range sum' do
+    arr = 0
+
+    _(solution(0)).must_equal 0
   end
 end
