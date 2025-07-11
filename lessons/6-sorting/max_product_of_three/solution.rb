f4df544e759276e22c8fdc0315ac7dc01680a3a6
Bui The Hoa <buithehoa@gmail.com>
2025-07-11 11:47:25 +0700

    Fix reported bugs.

--- lessons/6-sorting/max_product_of_three/solution.rb
+++ lessons/6-sorting/max_product_of_three/solution.rb
@@ -5,7 +5,12 @@ def solution(a)
   a.sort!
 
   max1 = a[-1] * a[-2] * a[-3]
-  max2 = a[0] * a[1] * a[2]
+
+  max2 = 0
+  temp_max = a[0] * a[1]
+  if temp_max > 0
+    max2 = temp_max * a[-1]
+  end
 
   return max1 > max2 ? max1 : max2
 end
--- lessons/6-sorting/max_product_of_three/solution_test.rb
+++ lessons/6-sorting/max_product_of_three/solution_test.rb
@@ -6,14 +6,20 @@ Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 describe 'solution' do
   it 'calculates the range sum' do
-    arr = [ -3, 1, 2, -2, 5, 6 ]
+    arr = [-3, 1, 2, -2, 5, 6]
 
     _(solution(arr)).must_equal 60
   end
 
   it 'handles negative numbers' do
-    arr = [ -9, 1, 2, -8, 5, 6 ]
+    arr = [-9, 1, 2, 3, 5, 6]
 
-    _(solution(arr)).must_equal 72
+    _(solution(arr)).must_equal 90
+  end
+
+  it 'handles negative numbers' do
+    arr = [-5, 5, -5, 4]
+
+    _(solution(arr)).must_equal 125
   end
 end
